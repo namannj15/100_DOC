7c0e8aad48398718cb2159ca34abe252b8afe5a2
NAMAN JAIN <77778261+namannj15@users.noreply.github.com>
2021-10-03 23:15:55 +0530

    graph undetected

---
+++ October/03.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+   vector<bool> vis;
+bool ans;
+void dfs(vector<int> adj[], int v, int p) {
+    vis[v] = true;
+    for (auto x : adj[v]) {
+        if (x == p)continue;
+        if (vis[x]) {
+            ans = true;
+            return;
+        }
+        else dfs(adj, x, v);
+    }
+}
+bool isCycle(int n, vector<int> adj[]) {
+    vis.assign(n + 1, false);
+    ans = false;
+    for (int i = 0; i < n; i++)if (!vis[i])dfs(adj, i, -1);
+    return ans;
+}
+};
+
+// { Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}  // } Driver Code Ends
