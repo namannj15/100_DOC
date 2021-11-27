7e5bed0dd9f49d9ed7972e27501958cb682aac8a
NAMAN JAIN <77778261+namannj15@users.noreply.github.com>
2021-11-27 23:19:18 +0530

    easy

---
+++ November/27.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+	public:
+	int NthRoot(int n, int m)
+	{
+	    // Code here.
+	    int start = 1, end = m;
+	    while(start <= end) {
+	        int mid = (start + end) / 2;
+	        if(pow(mid, n) == m) return mid;
+	        else if(pow(mid, n) < m) start = mid + 1;
+	        else end = mid - 1;
+	    }
+	    return -1;
+	}  
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
+}  // } Driver Code Ends
